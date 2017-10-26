a4b1aea6a2dcd4f2d02ed6f4079271568514b3f5
Michał Sałaban <michal@salaban.info>
2017-10-26 04:41:05 +0200

    Break out JSON file creation, add stub for histograms

--- atendo/query.py
+++ atendo/query.py
@@ -1,6 +1,7 @@
 from decimal import Decimal
-from datetime import datetime
+from datetime import datetime, timedelta
 import operator
+from sqlalchemy.sql import func
 
 from .txndb import Txn, TxnStat
 
@@ -8,23 +9,83 @@ class Query(object):
     when = None
     _stats = None
 
-    def __init__(self, dbsession, periods=(60*60, 60*60*24, 60*60*24*7)):
+    def __init__(self, dbsession, periods, bins=20):
         self.dbsession = dbsession
         self.periods = periods
+        self.bins = bins
 
-    def fetch_stats(self, when=None):
-        self.when = int((when or datetime.now()).timestamp())
-        period = max(self.periods)
+    def get_timeline(self, prop):
+        if not self._stats:
+            self.fetch_data()
+        return {'timestamp': int(self.when.timestamp()), 'data': self.timelinegen(prop)}
+
+    def get_hists(self, prop, period):
+        try:
+            return self._hists[period][prop]
+        except KeyError:
+            return []
+
+    def fetch_data(self, when=None):
+        self.when = when or datetime.now()
+        maxperiod = max(self.periods.values())
+
         stats = self.dbsession.query(TxnStat) \
-            .filter(TxnStat.queried > self.when - period) \
+            .filter(TxnStat.queried > self.when - timedelta(seconds=maxperiod)) \
             .order_by(TxnStat.queried)
         self._stats = list(map(operator.attrgetter('__dict__'), stats.all()))
+
+        self._hists = {}
+        for label, period in self.periods.items():
+            self._hists[label] = self._mkhists(period)
 
     def timelinegen(self, key):
         for x in self._stats:
             yield (int(x['queried'].timestamp()), x[key])
 
-    def get_timeline(self, prop):
-        if not self._stats:
-            self.fetch_stats()
-        return {'timestamp': self.when, 'data': self.timelinegen(prop)}
+    def _mkhists(self, period):
+        hists = {}
+        hash_ids = set()
+        txns = self.dbsession.query(Txn) \
+            .filter(Txn.queried > self.when - timedelta(seconds=period)) \
+            .order_by(Txn.queried)
+
+        if txns.count() == 0:
+            return hists
+
+        # keep only one copy of each Txn, the first-seen one
+        unique_txns = []
+        for txn in txns.all():
+            if txn.hash_id in hash_ids:
+                continue
+            hash_ids.add(txn.hash_id)
+            unique_txns.append(txn)
+
+        stats = self.dbsession.query(
+                func.min(Txn.fee).label('minfee'),
+                func.max(Txn.fee).label('maxfee'),
+                func.min(Txn.size).label('minsize'),
+                func.max(Txn.size).label('maxsize'),
+                func.min(Txn.inputs).label('mininputs'),
+                func.max(Txn.inputs).label('maxinputs'),
+                func.min(Txn.outputs).label('minoutputs'),
+                func.max(Txn.outputs).label('maxoutputs'),
+                func.min(Txn.ring).label('minring'),
+                func.max(Txn.ring).label('maxring')) \
+            .filter(Txn.queried > self.when - timedelta(seconds=period)).one()
+
+        hists['ring'] = self._mkhist(
+            unique_txns,
+            'ring',
+            range(stats.minring, stats.maxring + 1))
+        hists['inputs'] = self._mkhist(
+            unique_txns,
+            'inputs',
+            range(stats.mininputs, stats.maxinputs + 1))
+        hists['outputs'] = self._mkhist(
+            unique_txns,
+            'outputs',
+            range(stats.minoutputs, stats.maxoutputs + 1))
+        return hists
+
+    def _mkhist(self, txns, key, bins):
+        return []
--- exportjson.py
+++ exportjson.py
@@ -10,12 +10,25 @@ from atendo.utils import get_config_or_usage
 
 config = get_config_or_usage()
 
-s = mksession(config['db_url'])
-q = Query(s)
-for prop in ('txns', 'sumfee', 'sumsize', 'avgsize', 'avgfee', 'avgfeeperkb'):
+
+def write_json(label, data):
     tmpfd, tmpname = tempfile.mkstemp(
-        prefix="{0}-".format(prop), suffix='.tmp.json', dir=config['output_dir'])
+        prefix="{0}-".format(label), suffix='.tmp.json', dir=config['output_dir'])
     tmpout = os.fdopen(tmpfd, 'w')
-    simplejson.dump(q.get_timeline(prop), tmpout, iterable_as_array=True)
+    simplejson.dump(data, tmpout, iterable_as_array=True)
     tmpout.close()
-    os.rename(tmpname, os.path.join(config['output_dir'], "{0}.json".format(prop)))
+    os.rename(tmpname, os.path.join(config['output_dir'], "{0}.json".format(label)))
+
+
+s = mksession(config['db_url'])
+periods = {
+    '1h': 60 * 60,
+    '1d': 60 * 60 * 24,
+#    '1w': 60 * 60 * 24 * 7
+}
+q = Query(s, periods)
+for prop in ('txns', 'sumfee', 'sumsize', 'avgsize', 'avgfee', 'avgfeeperkb'):
+    write_json("timeline-{0}".format(prop), q.get_timeline(prop))
+for period in periods.keys():
+    for prop in ('inputs', 'outputs', 'ring', 'fee', 'size'):
+        write_json("hist-{0}-{1}".format(prop, period), q.get_hists(prop, period))
